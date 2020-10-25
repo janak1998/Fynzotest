83a9d4a77a2b202d47e6925b73c9b80a9bf98490
janak bohra <janakbohra29@gmail.com>
2020-10-25 10:22:51 +0530

    added validation

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 
 function success(a) {
-    
-    
-    if( a == 5 ){
     var checkboxes = document.querySelectorAll('input[type="checkbox"]');
     var checkedOne = Array.prototype.slice.call(checkboxes).some(x => x.checked);
-    alert(checkedOne);
+    if( checkedOne && a == 5 ){
+        document.getElementById("button"+a).style.display = 'block'; 
     }
-    
-    document.getElementById("button"+a).style.display = 'block'; 
-    
+    else if(a != 5){
+        document.getElementById("button"+a).style.display = 'block'; 
+    }
+    else{
+        document.getElementById("button"+a).style.display = 'none'; 
+    }
 
 
 }
@@ -21,11 +22,11 @@ function nameCheck(){
     var uname= document.getElementById("name").value;
     if(uname.match(letters))
     {
-        document.getElementById('sec3').scrollIntoView();
+        document.getElementById('sec3').scrollIntoView({ behavior: 'smooth', block: 'center' });
     }
     else
     {
-      alert("Please enter only letters");
+        alert("Please enter only letters");
     }
 
 
@@ -34,5 +35,16 @@ function nameCheck(){
 }
 
 function next(num){
-    document.getElementById('sec'+num).scrollIntoView();
+    document.getElementById('sec'+num).scrollIntoView({ behavior: 'smooth', block: 'center' });
+    scrollIntoView(false);
+        
+}
+
+function submitForm(){
+    var val1 = document.getElementById().value;
+    var val2 = document.getElementById().value;
+    var val3 = document.getElementById().value;
+    var val4 = document.getElementById().value;
+    var val5 = document.getElementById().value;
+    
 }
